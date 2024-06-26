1abb2ad2f2e218e4ad2d974ca22c9c842d92633b
Sohaib-Ahmed869 <i211114@nu.edu.pk>
2024-06-26 14:54:13 +0500

    26 june

--- src/AdminScreens/dashboard/order.js
+++ src/AdminScreens/dashboard/order.js
@@ -1,64 +1,144 @@
 import React from "react";
+import { useState } from "react";
+import users from "../../Assets/users.png";
+import accounttype from "../../Assets/accounttypes.png";
+import verified from "../../Assets/verified.png";
+import countries from "../../Assets/countries.png";
+import { BiCircle } from "react-icons/bi";
+import { BiEdit } from "react-icons/bi";
+import { ImBin } from "react-icons/im";
 
-import { BsCart } from "react-icons/bs";
+const stats = [
+  {
+    image: users,
+    title: "Total Orders",
+    value: 200,
+  },
+  {
+    image: accounttype,
+    title: "Completed Orders",
+    value: "3",
+  },
+  {
+    image: verified,
+    title: "Pending Orders",
+    value: 150,
+  },
+  {
+    image: countries,
+    title: "Cancelled Orders",
+    value: 20,
+  },
+];
 
-const orderStats = [
+const tableDate = [
   {
-    title: "Total",
-    value: 0,
+    number: "01",
+    name: "John Doe",
+    address: "Berliner Platz 9, 35390 Gießen",
+    email: "abcd@example.com",
+    role: "Vendor",
+    activity: "Visting",
   },
   {
-    title: "Completed",
-    value: 0,
+    number: "02",
+    name: "John Doe",
+    address: "Berliner Platz 9, 35390 Gießen",
+    email: "abcd@example.com",
+    role: "Vendor",
+    activity: "Visting",
   },
   {
-    title: "Pending",
-    value: 0,
+    number: "03",
+    name: "John Doe",
+    address: "Berliner Platz 9, 35390 Gießen",
+    email: "abcd@example.com",
+    role: "Vendor",
+    activity: "Visting",
   },
   {
-    title: "Processing",
-    value: 0,
+    number: "04",
+    name: "John Doe",
+    address: "Berliner Platz 9, 35390 Gießen",
+    email: "abcd@example.com",
+    role: "Vendor",
+    activity: "Visting",
   },
   {
-    title: "Cancelled",
-    value: 0,
+    number: "05",
+    name: "John Doe",
+    address: "Berliner Platz 9, 35390 Gießen",
+    email: "abcd@example.com",
+    role: "Vendor",
+    activity: "Visting",
   },
   {
-    title: "Refunded",
-    value: 0,
-  },
-  {
-    title: "On hold",
-    value: 0,
+    number: "06",
+    name: "John Doe",
+    address: "Berliner Platz 9, 35390 Gießen",
+    email: "abcd@example.com",
+    role: "Vendor",
+    activity: "Visting",
   },
 ];
 
 const Order = () => {
   return (
-    <div className="px-3 flex gap-3">
-      <div className="items-center mt-5 w-1/2 border-2 p-1">
-        <div className="border-b border-gray-300 p-2 justify-between flex">
-          <BsCart className="w-6 h-6 mr-2" />
-          <p className="text-xl  text-slate-900">Orders</p>
-        </div>
-        {orderStats.map((item, index) => (
-          <div
-            key={index}
-            className="border-b border-gray-300 p-2 justify-between flex"
-          >
-            <p className="text-sm  text-slate-900">{item.title}</p>
-            <p className="text-sm  text-slate-500">{item.value}</p>
+    <div className="p-10 pt-0">
+      <div className="flex flex-row items-center gap-15 p-5 shadow-md rounded-md bg-white">
+        {stats.map((item, index) => (
+          <div key={index} className="flex flex-row items-center w-1/4">
+            <img src={item.image} alt="users" className="w-12 h-12" />
+            <div className="flex flex-col ml-3">
+              <p className="text-xl  text-gray-800">{item.value}</p>
+              <p className="text-sm  text-slate-  900">{item.title}</p>
+            </div>
           </div>
         ))}
       </div>
-      <div className="items-center mt-5 w-1/2 border-2 p-1">
-        <div className="border-b border-gray-300 p-2 justify-between flex">
-          <BsCart className="w-6 h-6 mr-2" />
-          <p className="text-xl  text-slate-900">New Orders</p>
-        </div>
-        <div className="items-center flex justify-center mt-20">
-          <p className="text-2xl  text-slate-900">No new orders found</p>
-        </div>
+      <div className="flex-col gap-3 mt-5 table w-full">
+        <table className="w-full table-auto">
+          <thead>
+            <tr className="bg-black text-white">
+              <th className="border border-gray-300 p-2">S.No</th>
+              <th className="border border-gray-300 p-2">Name</th>
+              <th className="border border-gray-300 p-2">Address</th>
+              <th className="border border-gray-300 p-2">Email</th>
+              <th className="border border-gray-300 p-2">Role</th>
+              <th className="border border-gray-300 p-2">Activity</th>
+              <th className="border border-gray-300 p-2">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tableDate.map((item, index) => (
+              <tr key={index} className="text-gray-600">
+                <td className="border border-gray-300 p-2">{item.number}</td>
+                <td className="border border-gray-300 p-2">
+                  <div className="flex flex-row items-center gap-3">
+                    <p className="text-md">{item.name}</p>
+                  </div>
+                </td>
+                <td className="border border-gray-300 p-2">{item.address}</td>
+                <td className="border border-gray-300 p-2">{item.email}</td>
+                <td className="border border-gray-300 p-2">{item.role}</td>
+                <td className="border border-gray-300 p-2 items-center">
+                  <div className="flex items-center">
+                    <BiCircle className="mr-1 text-blue-500" color="blue" />
+                    {item.activity}
+                  </div>
+                </td>
+                <td className="flex flex-row items-center gap-3 border border-gray-300 p-5">
+                  <div className="bg-gray-200 rounded p-1">
+                    <BiEdit className="text-black" />
+                  </div>
+                  <div className=" bg-red-400 rounded p-1">
+                    <ImBin className="text-black" />
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
--- src/Layout.js
+++ src/Layout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import Home from "./Home/page";
 import About from "./About/page";
 import Categories from "./Categories/page";
@@ -18,27 +18,26 @@ import OTPVerification from "./AdminScreens/OTPverification";
 import NewPassword from "./AdminScreens/newPassword";
 import AdminDashboard from "./AdminScreens/dashboard/main";
 
-
 const Layout = () => {
   return (
     <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/careers" element={<Careers />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/registeration" element={<RegisterStart />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/multistep" element={<MultiStep />} />
-        <Route path="/dashboard" element={<Dashboard />} /> 
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/categories" element={<Categories />} />
+      <Route path="/careers" element={<Careers />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/registeration" element={<RegisterStart />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/multistep" element={<MultiStep />} />
+      <Route path="/dashboard" element={<Dashboard />} />
 
-        <Route path="/admin/login" element={<AdminLogin />} />
-        <Route path="/admin/forgetpassword" element={<ForgetPassword />} />
-        <Route path="/admin/otpverification" element={<OTPVerification />} />
-        <Route path="/admin/newpassword" element={<NewPassword />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+      <Route path="/admin/login" element={<AdminLogin />} />
+      <Route path="/admin/forgetpassword" element={<ForgetPassword />} />
+      <Route path="/admin/otpverification" element={<OTPVerification />} />
+      <Route path="/admin/newpassword" element={<NewPassword />} />
+      <Route path="/admin/dashboard" element={<AdminDashboard />} />
     </Routes>
   );
 };
